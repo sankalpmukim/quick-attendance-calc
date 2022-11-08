72237a456162e038767a1ae57c81b88764577a4b
Sankalp Mukim <sankalpmukim@gmail.com>
2022-11-08 14:27:30 +0530

    working kinda

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
+import nextPWA from "next-pwa";
 // @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+
+const withPWA = nextPWA({
+  dest: "public",
+});
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,4 +19,4 @@ const config = {
     defaultLocale: "en",
   },
 };
-export default config;
+export default withPWA(config);
